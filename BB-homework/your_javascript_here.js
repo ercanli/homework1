2a62afeed1e2b677739c2d714ea213ee431879ed
ercanli <oercanli@gmail.com>
2018-04-29 23:03:46 +0200

    final update

--- BB-homework/your_javascript_here.js
+++ BB-homework/your_javascript_here.js
@@ -1,5 +1,73 @@
 // Variables
 
+
+var hero = {name: 'osman',
+            heroic: (true) ,
+            inventory: [],
+            health: 10,
+            weapon: {type: 'gun', damage: 5}
+            };
+
+//functions
+
+function rest(creature) {
+  health = 10;
+  return creature;
+}
+
+console.log(rest(5));
+
+
+
+function pickUpItem(creature, item) {
+var newLength = inventory.push ('item')
+return creature
+}
+
+
+function dealDamage(attacker, defender){
+var defender = {};
+var obj = attacker[damage]
+var obj2 = defender[health]
+// is this possible at all? --->> health.defender - damage.weapon.attacker
+defender [health] = obj2 - obj;
+return defender
+}
+
+
+
+function equipWeapon(creature, index) {
+var newweapon = creature[weapon]
+var index = inventory.index0;
+array.splice(index, 1);
+
+}
+
+
+function doBattle(heroicCreature, creature) {
+if (heroicCreature.heroic != (true))
+return 'null'
+while (health.heroicCreature != 0 && health.creature != 0)
+dealDamage.heroicCreature;
+dealDamage.creature;
+return health;
+if (health.heroicCreature <= 0);
+window.alert('your hero is dead');
+return health.heroicCreature;
+}
+
+
+
+
+
+
 // Game logic
+
+
+var bed = document.getElementById('onclick-bed');
+bed.onclick = function(rest) {
+    console.log('rest gets called correctly');
+}
+
 
 // UI
